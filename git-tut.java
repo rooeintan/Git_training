bfdf8530f665d60c7a135de119884f611f65d3e8
roeintan <roeintan.shz@gmail.com>
2017-11-27 17:44:48 +0330

    branch1

---
+++ git-tut.java
@@ -0,0 +1,18 @@
+package www.roeinapp.ir
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by sama on 11/27/2017.
+ */
+public class HistoryCar extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history_car);
+    
+ 	//hello world
+
+    }
+}
